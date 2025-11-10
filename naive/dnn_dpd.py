68e07168187df1f30954460e1ec7acc34701b91f
shiyi0113 <shiyileshengxia@gmail.com>
2025-11-10 13:34:39 +0800

    adjustment

--- naive/dnn_dpd.py
+++ naive/dnn_dpd.py
@@ -8,6 +8,8 @@ import pandas as pd
 import torch.nn as nn
 import multiprocessing
 from utils.visualization import plot_metrics
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_DIR = os.path.join(SCRIPT_DIR, os.pardir, 'model')
@@ -21,26 +23,32 @@ print(f'Using device:{device}')
 # 数据准备
 df = pd.read_csv(os.path.join(SCRIPT_DIR, os.pardir, 'data/diabetes_prediction_dataset.csv'))
 # 对分类列进行独热编码
-gender_dummies = pd.get_dummies(df['gender'], prefix='gender')
-smoking_history_dummies = pd.get_dummies(df['smoking_history'],prefix='smoking_history')
-df = pd.concat([df.drop(['gender', 'smoking_history'],axis=1), gender_dummies, smoking_history_dummies], axis=1)
+categorical_cols = ['gender', 'smoking_history']
+df = pd.get_dummies(df, columns=categorical_cols, drop_first=True)
 
-arr = df.values
-arr =arr.astype(np.float32)
-ts = torch.tensor(arr).to(device)
+X = df.drop(columns=['diabetes'])
+Y = df['diabetes']
+continuous_cols = ['age', 'bmi', 'HbA1c_level', 'blood_glucose_level']
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42, stratify=Y) 
+scaler = StandardScaler()
+X_train[continuous_cols] = scaler.fit_transform(X_train[continuous_cols])
+X_test[continuous_cols] = scaler.transform(X_test[continuous_cols])
 
-train_size = int(ts.size(0)*0.7)
-test_size = int(ts.size(0)*0.3)
-ts = ts[torch.randperm(ts.size(0)),:]
-train_Data = ts[:train_size,:]
-test_Data = ts[train_size:,:]
+train_df = pd.concat([X_train, Y_train], axis=1)
+test_df = pd.concat([X_test, Y_test], axis=1)
+arr_train = train_df.values.astype(np.float32)
+arr_test = test_df.values.astype(np.float32)
+train_Data = torch.tensor(arr_train).to(device)
+test_Data = torch.tensor(arr_test).to(device)
 
 # 模型
 class DNN(nn.Module):
     def __init__(self):
         super(DNN,self).__init__()
         self.net = nn.Sequential(
-            nn.Linear(15,32),nn.ReLU(),
+            nn.Linear(13,32),nn.ReLU(),
             nn.Linear(32,8),nn.ReLU(),
             nn.Linear(8,4),nn.ReLU(),
             nn.Linear(4,1)
@@ -55,7 +63,7 @@ loss_fn = nn.BCEWithLogitsLoss(reduction='mean')
 optimizer = torch.optim.Adam(model.parameters(),lr = 0.005)
 
 def train_model():
-    epochs = 1000
+    epochs = 100
     train_losses = []
     train_accuracies = []
     X = train_Data[:,:-1]
@@ -68,11 +76,11 @@ def train_model():
         loss.backward()
         optimizer.step()
 
-        Pred[Pred>0.5]=1
-        Pred[Pred<=0.5]=0
+        Pred[Pred>0.0] = 1.0
+        Pred[Pred<=0.0] = 0.0
         corrent = torch.sum(Pred==Y)
         total = Y.size(0)
-        accuracies = corrent.float()/total
+        accuracies = corrent/total
         train_losses.append(loss.item())
         train_accuracies.append(accuracies.item())
 
@@ -88,11 +96,11 @@ def test_model():
     model.eval()
     with torch.no_grad():
         Pred = model(X)
-        Pred[Pred>0.5]=1
-        Pred[Pred<=0.5]=0
+        Pred[Pred>0.0]=1.0
+        Pred[Pred<=0.0]=0.0
         corrent = torch.sum(Pred==Y)
         total = Y.size(0)
-    print(f'测试准确率：{corrent.float()/total}')
+    print(f'测试准确率：{corrent/total}')
 
 if __name__ == '__main__':
     multiprocessing.set_start_method('spawn')
